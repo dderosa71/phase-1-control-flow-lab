191a5ea66c9de39755273c157618b8543ceb3c7f
Dominic DeRosa <dderosa71@gmail.com>
2021-07-13 20:30:17 -0500

    commit tree 2648779dbe30e75fd1aa4e8e14bf0710a3ad1d3f

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
+  if (x < 400){
+    return 'This one is on me!'
+  }
+  else if (x > 2500){
+    return 'No can do.'
+  }
+  else if (x > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
+  return x === 'NYC' ?  "Ok, sounds good." : "No go.";
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(x){
+    if (x === 'generous'){
+      return 'Thank you so much.'
 }
+    else if (x === 'not as generous'){
+      return 'Thank you.'
+    }
+    else if ( x === 'thanks for everything'){
+      return 'Bye.'
+    }
+}  
+  
